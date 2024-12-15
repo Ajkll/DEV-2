dbab4f8aceabf3c1c07c8921d9b7191bffcfb104
PasRP-Theo <theomertens12345@gmail.com>
2024-12-15 20:51:59 +0100

    Add files via upload

---
+++ projet/tileset/maison_toad.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="maison_toad" tilewidth="16" tileheight="16" tilecount="6" columns="2">
+ <image source="maison_toad.png" trans="000000" width="32" height="48"/>
+</tileset>
---
+++ projet/tileset/recul.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="recul" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="recul.png" trans="0080ff" width="17" height="17"/>
+</tileset>
