cf175a58f9a602f78978f7e9f5cfd3b26f73e185
PasRP-Theo <theomertens12345@gmail.com>
2024-12-27 10:14:32 +0100

    Update test_pion_theo.py

--- test/test_pion_theo.py
+++ test/test_pion_theo.py
@@ -37,19 +37,19 @@ class TestPion(unittest.TestCase):
         self.assertEqual(pion.position, 0)
 
     def test_sur_case(self):
-        pion = Pion("Pion 1")
+        pion = Pion("Pion 2")
         pion.deplacer(5)
         self.assertTrue(pion.est_sur_case(5))
         self.assertFalse(pion.est_sur_case(4))
 
     def test_sur_case_limites(self):
-        pion = Pion("Pion 1")
+        pion = Pion("Pion 2")
         pion.deplacer(100)
         self.assertTrue(pion.est_sur_case(100))
         self.assertFalse(pion.est_sur_case(99))
 
     def test_grands_deplacements(self):
-        pion = Pion("Pion 1")
+        pion = Pion("Pion 2")
         pion.deplacer(1_000_000)
         self.assertEqual(pion.position, 1_000_000)
         pion.reculer(500_000)
